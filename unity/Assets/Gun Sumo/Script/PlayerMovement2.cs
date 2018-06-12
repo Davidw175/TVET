9c7c6e765700c854a6b040b4877e63ca547c1af8
davidw`75 <davidwang1775@gmail.com>
2018-06-12 17:59:08 +1000

    created move script

---
+++ unity/Assets/Gun Sumo/Script/PlayerMovement2.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement2 : MonoBehaviour
+{
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        /*float moveHorizontal = Input.GetAxis("Horizontal2");
+        float moveVertical = Input.GetAxis("Vertical2");
+
+        Vector3 movement = new Vector3(moveVertical, 0.0f, moveHorizontal);
+
+        rb.AddForce(movement * speed);*/
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.MovePosition(transform.position + transform.right * -speed);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.MovePosition(transform.position + transform.right * speed);
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rb.MovePosition(transform.position + transform.up * speed);
+            //rb.AddForce(transform.position + transform.up * speed);
+        }
+        if (speed <= 0.15)
+        {
+            speed = 0.15f;
+        }
+    }
+}
